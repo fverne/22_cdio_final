ff9182fc57c21f31fc23c3701a27fb0e9eb7f790
Alexander <alexanderlambrecht30@gmail.com>
2020-01-07 13:23:59 +0100

    deck er singleton

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,15 @@
-
+//https://www.geeksforgeeks.org/singleton-class-java/ singleton kode hentet her fra
 
 public class Deck {
     private static Deck single_instance = null;
+
+    public static Deck getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Deck();
+
+        return single_instance;
+    }
+}
 
 }
