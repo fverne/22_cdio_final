7146ed64b5727e0534f3a635f33ba809963d7496
fverne <frederikverne95@gmail.com>
2020-01-08 13:03:00 +0100

    Die and Roll classes added

--- src/main/java/model/Die.java
+++ src/main/java/model/Die.java
@@ -1,4 +1,15 @@
 package model;
+import model.*;
 
 public class Die {
+    private int faceValue;
+
+    public int rollDie() {
+        faceValue = (int) (Math.random() * 6 + 1);
+        return faceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
 }
---
+++ src/main/java/model/Roll.java
@@ -0,0 +1,16 @@
+package model;
+import model.*;
+
+public class Roll {
+    public Die die;
+
+    public Roll(){
+        die = new Die();
+        die.rollDie();
+    }
+
+    public int getRoll(){
+        int roll = die.getFaceValue();
+        return roll;
+    }
+}
