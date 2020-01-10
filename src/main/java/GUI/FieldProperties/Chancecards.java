3bc340e4fc649aedfe5a2c4765fa3c9789644ecb
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2020-01-10 12:28:55 +0100

    Change border and subtext according to owner
    added a display chancecard method

--- src/main/java/GUI/FieldProperties/Chancecards.java
+++ src/main/java/GUI/FieldProperties/Chancecards.java
@@ -64,6 +64,7 @@ public class Chancecards extends Fields {
     public String getMessage() {
         return message;
     }
+
     @Override
     public Fields landOnField(Player player) {
         Chancecards card = Chancecards.getRandomCard();
